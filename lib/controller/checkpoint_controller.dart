78c8d0b56c92687b8393c2c4c3cef02ac866daa8
Shanjinur Islam <spondoncsebuet@gmail.com>
2019-10-31 18:04:26 +0600

    Landing UI Done

---
+++ lib/controller/checkpoint_controller.dart
@@ -0,0 +1,1 @@
+//fetch checkpoint of a task
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:elo_programming_task/view/home_ui.dart';
+import 'package:elo_programming_task/view/task_over_ui.dart';
+import 'package:elo_programming_task/view/task_ui.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Distance Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      routes: {
+        '/': (context) => HomeUI(),
+        '/task': (context) => TaskUI(),
+        '/finish': (context) => TaskOverUI(),
+      },
+    );
+  }
+}
+
+
